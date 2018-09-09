bbb59e3cf0614fa5a1798f031ee54c9fe280c3b3
Alan Shum <alan200994@gmail.com>
2018-09-09 15:05:02 -0700

    clean code + caching

--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ import Router from 'koa-router';
 import cors from '@koa/cors';
 import dotEnv from 'dotenv';
 
+import Cache from './library/cache';
+import PortfolioAPI from './library/portfolio';
 import corsConfig from '../cors.json';
-import PortfolioAPI from './portfolio';
 
 dotEnv.config();
 
+if (!process.env.ROBINHOOD_TOKEN) throw new Error('You need to supply a robinhood access token to start the server');
+
+const requestCache = (new Cache(process.env.ROBINHOOD_TOKEN, 'request')).init();
 const app = new Koa();
-const portfolioAPI = new PortfolioAPI(process.env.ROBINHOOD_TOKEN, { testMode: true });
+const portfolioAPI = new PortfolioAPI(process.env.ROBINHOOD_TOKEN, { testMode: false });
 const router = Router();
 const port = 3000;
 
 router.get('/portfolio', async (ctx) => {
+  const cachedPortfolio = requestCache.get('portfolio');
+  if (cachedPortfolio) {
+    ctx.body = JSON.stringify(cachedPortfolio);
+    return;
+  }
+
   const portfolio = await portfolioAPI.get();
+  requestCache.save('portfolio', portfolio);
   ctx.body = JSON.stringify(portfolio);
 });
 
---
+++ src/library/cache.js
@@ -0,0 +1,48 @@
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+import { writeFileSync, existsSync } from 'fs';
+import { resolve } from 'path';
+import crypto from 'crypto';
+
+export default class Cache {
+  constructor(id, cacheType, ttl = 3600 * 24 * 1000) {
+    this.id = id;
+    this.cacheType = cacheType;
+    this.ttl = ttl;
+  }
+
+  init() {
+    const cachePath = resolve(`.cache/${this.cacheFileName}.json`);
+    if (!existsSync(cachePath)) writeFileSync(cachePath, '');
+    const adapter = new FileSync(cachePath);
+    this.db = low(adapter);
+    return this;
+  }
+
+  get cacheFileName() {
+    const id = crypto.createHash('md5').update(this.id).digest('hex');
+    return `${this.cacheType}_${id}`.replace(/\./g, '_');
+  }
+
+  save(key, data) {
+    return this.db
+      .set(Cache.encodeKey(key), {
+        created: new Date(),
+        data,
+      })
+      .write();
+  }
+
+  get(key) {
+    const cachedData = this.db
+      .get(Cache.encodeKey(key))
+      .value();
+
+    if (!cachedData || new Date() - cachedData.created > this.ttl) return null;
+    return cachedData.data;
+  }
+
+  static encodeKey(key) {
+    return encodeURIComponent(key).replace(/\./g, '_');
+  }
+}
